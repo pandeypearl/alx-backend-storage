c506eb8cacb8cd0d67133762c1fb9aed4b85ef34
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-12 02:53:38 -0700

    Advanced Task 12. Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,10 @@
+-- Creates a stored procedure ComputeAverageWeightedScoreForUser
+-- Computes and stores average weighted score for student
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+BEGIN
+UPDATE users
+SET average_score = (SELECT SUM(score * (SELECT weight FROM projects WHERE id = corrections.project_id)) / (SELECT sum(weight FROM projects) FROM corrections WHERE corrections.user_id = user_id) WHERE id = user_id;
+END//
+DELIMITER ;
