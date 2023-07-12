137d7e37ef9c0d054e2c8472d3d2cca1dc0cca92
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-12 01:15:32 -0700

    Task 1. In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,11 @@
 -- Creates table users with following atrributes
 --	id: integer, never null, auto increment and primary key
 --	email: string(255 chars), never null and unique
---	name: string(255 chars)
+--	name: string(255 chars),
+--	country, enumeration of countries: US, CO, TN, never null, default US
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
