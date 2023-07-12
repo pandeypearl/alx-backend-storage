5e6351f6c721cdac4dcad87712dc1021bd10657f
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-12 09:27:27 -0700

    Uding correct version of sum for Advanced Task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,6 +5,6 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
 UPDATE users
-SET average_score = (SELECT SUM(score * (SELECT weight FROM projects WHERE id = corrections.project_id)) / (SELECT sum(weight FROM projects) FROM corrections WHERE corrections.user_id = user_id) WHERE id = user_id;
+SET average_score = (SELECT SUM(score * (SELECT weight FROM projects WHERE id = corrections.project_id)) / (SELECT SUM(weight FROM projects) FROM corrections WHERE corrections.user_id = user_id) WHERE id = user_id;
 END//
 DELIMITER ;
