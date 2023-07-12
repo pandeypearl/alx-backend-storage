a33ceaa2b32423c226653257fa738c8baa7b7fcf
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-12 09:21:43 -0700

    Correcting PRIMARY_KEY assignment to id in Task 0 and Task 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@
 --	email: string(255 chars), never null and unique
 --	name: string(255 chars)
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@
 --	name: string(255 chars),
 --	country, enumeration of countries: US, CO, TN, never null, default US
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
