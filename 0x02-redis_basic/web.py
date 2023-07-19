d0fe82205491f273d1e2cb497756b774d04681e6
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-19 15:26:03 +0000

    Adding decorator to task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,30 +4,41 @@
 import redis
 import requests
 from functools import wraps
-from typing import Callable
+from typing import Dict
 import time
 
-def data_cacher(fn: Callable) -> Callable:
+
+def data_cacher(exp_time: int):
     """ Caches output of fetched data """
-    @wraps(fn)
-    def invoker(url: str) -> str:
-        """Wrapper function for caching output"""
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        result = client.get(f'{url}')
-        if result:
-            return result.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return invoker
+    def decorator(func):
+        result = {}
+
+        @wraps(func)
+        def invoker(url):
+            """Wrapper function for caching output"""
+            if url in result and time.time() - \
+                    result[url]['timestamp'] < exp_time:
+                result[url]['count'] += 1
+                return result[url]['content']
+
+            content = func(url)
+            result[url] = {
+                'content': content,
+                'timestamp': time.time(),
+                'count': 1
+            }
+            return result
+
+        return invoker
+
+    return decorator
 
 
-@data_cacher
+@data_cacher(exp_time=10)
 def get_page(url: str) -> str:
     """Returns content of a URL after caching the request's
     response, and tracking the request"""
-    response = requests.get(url, timeout=10)
+    response = requests.get(url)
     return response.text
 
 
@@ -35,4 +46,4 @@ if __name__ == '__main__':
     url = ('http://slowwly.robertomurray.co.uk/delay/1000/url')
     for _ in range(5):
         print(get_page(url))
-        time.sleep(2)
+        time.sleep(3)
