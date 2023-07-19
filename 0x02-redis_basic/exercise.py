f2de497d7dba084ecb51e0b180d1e473b007c0df
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-19 09:25:10 +0000

    Task1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Writting strings to Redis """
 
-from typing import Union
+from typing import Union, Optional, Union
 import redis
 import uuid
 
@@ -19,3 +19,17 @@ class Cache:
         self._redis.set(id, data)
         return id
 
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """ Converst data back to the desired format """
+        val = self._redis.get(key)
+        return val if not fn else fn(val)
+
+    def get_str(self, key):
+        """ Parametrize Cache.get with
+        correct(str) conversion function """
+        return self.get(key, str)
+
+    def get_int(self, key):
+        """ Parametrize Cache.get with
+        correct(int) conversion function """
+        return self.get(key, int)
