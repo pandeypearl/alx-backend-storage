ea6a4e6ae894b683a9564dc3620c23f0195b78cd
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-19 10:27:05 +0000

    Code fix : typos in Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,7 +7,8 @@ from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.redis()
+redis_store = redis.Redis()
+""" Module-level Redis instance """
 
 
 def data_cacher(methos: Callable) -> Callable:
@@ -18,7 +19,7 @@ def data_cacher(methos: Callable) -> Callable:
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
-            return result.decode('utl-8')
+            return result.decode('utf-8')
         result = method(url)
         redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
